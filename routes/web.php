3f0b32593b9d7ead5eee44eee84d0d7a2f6d3b52
Sajed Saidi <programmingstudent8@gmail.com>
2025-02-23 23:10:03 +0200

    Clean and start

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/test', function () {
-    return response()->json(['message' => 'Hello, World!']);
-});
-
-Route::get('/login', function () {
-    return response()->json(['message' => 'Login here!']);
+    return 'Hello world!';
 });
 
 Route::fallback(function () {
