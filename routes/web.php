e5449a7777aca545f7f153651162ffa22143a881
Sajed Saidi <programmingstudent8@gmail.com>
2025-03-02 17:22:09 +0200

    add test route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
     return 'Hello world!';
 });
 
+Route::get('/test', function () {
+    return 'test!';
+});
+
 Route::fallback(function () {
     return response()->json(['message' => 'Page not found!'], 404);
 });
