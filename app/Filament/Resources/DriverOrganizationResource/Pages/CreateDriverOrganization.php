60a19e02275f7aece219eeac5781aeb0703e39f8
adam-Sar <adamsaraya321@gmail.com>
2025-04-07 20:56:21 +0300

    before bus resource auto org

--- app/Filament/Resources/DriverOrganizationResource/Pages/CreateDriverOrganization.php
+++ app/Filament/Resources/DriverOrganizationResource/Pages/CreateDriverOrganization.php
@@ -4,8 +4,18 @@ namespace App\Filament\Resources\DriverOrganizationResource\Pages;
 
 use App\Filament\Resources\DriverOrganizationResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Auth;
 
 class CreateDriverOrganization extends CreateRecord
 {
     protected static string $resource = DriverOrganizationResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        if (Auth::user()->role === 'manager') {
+            $data['organization_id'] = Auth::user()->organization_id;
+        }
+        
+        return $data;
+    }
 } 
